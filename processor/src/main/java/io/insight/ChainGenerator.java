2dea283825ec8b7b5398582c7e970dfaea47cc22
Thibault Chaix <tch@nc>
2018-08-06 11:40:34 +0200

    Chore: Remove example unused module

--- processor/src/main/java/io/insight/ChainGenerator.java
+++ processor/src/main/java/io/insight/ChainGenerator.java
@@ -1,78 +1,85 @@
 package io.insight;
 
-import com.squareup.javapoet.*;
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.ParameterizedTypeName;
+import com.squareup.javapoet.TypeName;
+import com.squareup.javapoet.TypeSpec;
 
-import javax.lang.model.element.Modifier;
-import javax.lang.model.element.TypeElement;
-import javax.lang.model.element.VariableElement;
-import javax.lang.model.type.TypeKind;
-import javax.lang.model.util.ElementFilter;
 import java.io.IOException;
 import java.io.Writer;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.TypeKind;
+import javax.lang.model.util.ElementFilter;
+
 public class ChainGenerator {
-  private final String packageName;
-  private final String originClassName;
-  private final TypeElement element;
-  private String itfName;
+    private final String packageName;
 
-  ChainGenerator(String packageName, TypeElement element, String itfName) {
+    private final String originClassName;
 
-    this.packageName = packageName;
-    this.originClassName = element.getSimpleName().toString();
-    this.element = element;
-    this.itfName = itfName;
-  }
+    private final TypeElement element;
 
-  void writeTo(Writer writer) throws IOException {
-    JavaFile javaFile = null;
-    try {
-      javaFile = JavaFile.builder(packageName, build()).build();
-    } catch (Exception e) {
-      e.printStackTrace();
+    private final String itfName;
+
+    ChainGenerator(final String packageName, final TypeElement element, final String itfName) {
+
+        this.packageName = packageName;
+        this.originClassName = element.getSimpleName().toString();
+        this.element = element;
+        this.itfName = itfName;
     }
-    javaFile.writeTo(writer);
-  }
 
-  private TypeSpec build() {
-    TypeSpec.Builder classBuilder = TypeSpec.classBuilder(className())
-        .superclass(ParameterizedTypeName.get(ClassName.get(ServiceChain.class),
-            ClassName.get(packageName, itfName)))
-        .addSuperinterface(ClassName.get(element.asType()))
-        .addModifiers(Modifier.PUBLIC);
+    void writeTo(final Writer writer) throws IOException {
+        JavaFile javaFile = null;
+        try {
+            javaFile = JavaFile.builder(packageName, build()).build();
+            javaFile.writeTo(writer);
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-    ElementFilter.methodsIn(element.getEnclosedElements()).forEach(me -> {
-      TypeName returnType = TypeName.get(me.getReturnType());
-      MethodSpec.Builder methodBuilder = MethodSpec.methodBuilder(me.getSimpleName().toString())
-          .addModifiers(Modifier.PUBLIC)
-          .returns(returnType);
-      me.getParameters().forEach(ve ->
-          methodBuilder.addParameter(TypeName.get(ve.asType()), ve.getSimpleName().toString()));
-       
-      List<String> paramNames = me.getParameters().stream().map(ve -> ve.getSimpleName().toString())
-          .collect(Collectors.toList());
-      me.getThrownTypes().forEach(et -> methodBuilder.addException(TypeName.get(et)));
-      ClassName invocationType = ClassName.get(packageName, itfName, JavaPoetUtils.upperCaseFirst(me.getSimpleName().toString()) + "Invocation");
-      if (me.getReturnType().getKind() == TypeKind.VOID) {
-        methodBuilder.addStatement("new $T(root.get() $L).next()", invocationType,
-            (paramNames.isEmpty()? "" : ", ") + String.join(", ", paramNames));
-      } else {
-        methodBuilder.addStatement("return new $T(root.get() $L).next()", invocationType,
-            (paramNames.isEmpty()? "" : ", ") + String.join(", ", paramNames));
-      }
-      classBuilder.addMethod(methodBuilder.build());
+    private TypeSpec build() {
+        final TypeSpec.Builder classBuilder = TypeSpec.classBuilder(className()).superclass(
+                ParameterizedTypeName.get(ClassName.get(ServiceChain.class),
+                        ClassName.get(packageName, itfName))).addSuperinterface(
+                                ClassName.get(element.asType())).addModifiers(Modifier.PUBLIC);
 
-    });
-    return classBuilder.build();
-  }
+        ElementFilter.methodsIn(element.getEnclosedElements()).forEach(me -> {
+            final TypeName returnType = TypeName.get(me.getReturnType());
+            final MethodSpec.Builder methodBuilder = MethodSpec.methodBuilder(
+                    me.getSimpleName().toString()).addModifiers(Modifier.PUBLIC).returns(returnType);
+            me.getParameters().forEach(ve -> methodBuilder.addParameter(TypeName.get(ve.asType()),
+                    ve.getSimpleName().toString()));
 
-  String className() {
-    return originClassName + "Chain";
-  }
+            final List<String> paramNames = me.getParameters().stream().map(
+                    ve -> ve.getSimpleName().toString()).collect(Collectors.toList());
+            me.getThrownTypes().forEach(et -> methodBuilder.addException(TypeName.get(et)));
+            final ClassName invocationType = ClassName.get(packageName, itfName,
+                    JavaPoetUtils.upperCaseFirst(me.getSimpleName().toString()) + "Invocation");
+            if (me.getReturnType().getKind() == TypeKind.VOID) {
+                methodBuilder.addStatement("new $T(root.get() $L).next()", invocationType,
+                        (paramNames.isEmpty() ? "" : ", ") + String.join(", ", paramNames));
+            } else {
+                methodBuilder.addStatement("return new $T(root.get() $L).next()", invocationType,
+                        (paramNames.isEmpty() ? "" : ", ") + String.join(", ", paramNames));
+            }
+            classBuilder.addMethod(methodBuilder.build());
 
-  String getFullName() {
-    return packageName + "." + className();
-  }
+        });
+        return classBuilder.build();
+    }
+
+    String className() {
+        return originClassName + "Chain";
+    }
+
+    String getFullName() {
+        return packageName + "." + className();
+    }
 }
--- processor/src/main/java/io/insight/InterfaceGenerator.java
+++ processor/src/main/java/io/insight/InterfaceGenerator.java
@@ -1,111 +1,110 @@
 package io.insight;
 
-import com.squareup.javapoet.*;
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeName;
+import com.squareup.javapoet.TypeSpec;
 
-import javax.lang.model.element.Modifier;
-import javax.lang.model.element.TypeElement;
-import javax.lang.model.type.TypeKind;
-import javax.lang.model.util.ElementFilter;
 import java.io.IOException;
 import java.io.Writer;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.TypeKind;
+import javax.lang.model.util.ElementFilter;
+
 class InterfaceGenerator {
 
-  private final String packageName;
-  private final String originClassName;
-  private final TypeElement element;
+    private final String packageName;
 
-  InterfaceGenerator(String packageName, TypeElement element) {
+    private final String originClassName;
 
-    this.packageName = packageName;
-    this.originClassName = element.getSimpleName().toString();
-    this.element = element;
-  }
+    private final TypeElement element;
 
-  void writeTo(Writer writer) throws IOException {
-    JavaFile javaFile = null;
-    try {
-      javaFile = JavaFile.builder(packageName, build()).build();
-    } catch (Exception e) {
-      e.printStackTrace();
+    InterfaceGenerator(final String packageName, final TypeElement element) {
+
+        this.packageName = packageName;
+        this.originClassName = element.getSimpleName().toString();
+        this.element = element;
     }
-    javaFile.writeTo(writer);
-  }
 
-  private TypeSpec build() {
-    TypeSpec.Builder iface = TypeSpec.interfaceBuilder(className())
-        .addModifiers(Modifier.PUBLIC);
+    void writeTo(final Writer writer) throws IOException {
+        JavaFile javaFile = null;
+        try {
+            javaFile = JavaFile.builder(packageName, build()).build();
+            javaFile.writeTo(writer);
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    private TypeSpec build() {
+        final TypeSpec.Builder iface = TypeSpec.interfaceBuilder(className()).addModifiers(Modifier.PUBLIC);
 
+        ElementFilter.methodsIn(element.getEnclosedElements()).forEach(me -> {
+            final TypeName returnType = TypeName.get(me.getReturnType());
+            final String methodName = me.getSimpleName().toString();
+            final MethodSpec.Builder methodBuilder = MethodSpec.methodBuilder(methodName).addModifiers(
+                    Modifier.ABSTRACT, Modifier.PUBLIC).returns(returnType);
+            me.getParameters().forEach(ve -> methodBuilder.addParameter(TypeName.get(ve.asType()),
+                    ve.getSimpleName().toString()));
 
-    ElementFilter.methodsIn(element.getEnclosedElements()).forEach(me -> {
-      TypeName returnType = TypeName.get(me.getReturnType());
-      String methodName = me.getSimpleName().toString();
-      MethodSpec.Builder methodBuilder = MethodSpec.methodBuilder(methodName)
-          .addModifiers(Modifier.ABSTRACT, Modifier.PUBLIC)
-          .returns(returnType);
-      me.getParameters().forEach(ve ->
-          methodBuilder.addParameter(TypeName.get(ve.asType()), ve.getSimpleName().toString()));
+            final InvocationGenerator invocation = new InvocationGenerator(packageName, className(), me);
+            methodBuilder.addParameter(ClassName.get(packageName, className(), invocation.className()),
+                    "invocation");
 
-      InvocationGenerator invocation = new InvocationGenerator(packageName, className(), me);
-      methodBuilder.addParameter(ClassName.get(packageName, className(), invocation.className()), "invocation");
+            me.getThrownTypes().forEach(et -> methodBuilder.addException(TypeName.get(et)));
 
-      me.getThrownTypes().forEach(et -> methodBuilder.addException(TypeName.get(et)));
+            iface.addMethod(methodBuilder.build());
+            iface.addType(invocation.build());
+        });
+        iface.addMethod(
+                MethodSpec.methodBuilder("wrap").addModifiers(Modifier.STATIC, Modifier.PUBLIC).addParameter(
+                        ClassName.get(element.asType()), "service").returns(classType()).addStatement(
+                                "return $L", buildWrapClass()).build());
+        return iface.build();
+    }
 
-      iface.addMethod(methodBuilder.build());
-      iface.addType(invocation.build());
-    });
-    iface.addMethod(MethodSpec.methodBuilder("wrap")
-        .addModifiers(Modifier.STATIC,Modifier.PUBLIC)
-        .addParameter(ClassName.get(element.asType()), "service")
-        .returns(classType())
-        .addStatement("return $L", buildWrapClass())
-        .build());
-    return iface.build();
-  }
+    private TypeSpec buildWrapClass() {
+        final TypeSpec.Builder wrapClass = TypeSpec.anonymousClassBuilder("").addSuperinterface(classType());
+        ElementFilter.methodsIn(element.getEnclosedElements()).forEach(me -> {
+            final String methodName = me.getSimpleName().toString();
+            final TypeName returnType = TypeName.get(me.getReturnType());
 
-  private TypeSpec buildWrapClass(){
-    TypeSpec.Builder wrapClass = TypeSpec.anonymousClassBuilder("")
-        .addSuperinterface(classType());
-    ElementFilter.methodsIn(element.getEnclosedElements()).forEach(me -> {
-      String methodName = me.getSimpleName().toString();
-      TypeName returnType = TypeName.get(me.getReturnType());
+            final boolean isVoid = me.getReturnType().getKind() == TypeKind.VOID;
 
-      boolean isVoid = me.getReturnType().getKind() == TypeKind.VOID;
+            final MethodSpec.Builder methodBuilder = MethodSpec.methodBuilder(methodName).addModifiers(
+                    Modifier.PUBLIC).returns(returnType);
 
-      MethodSpec.Builder methodBuilder = MethodSpec.methodBuilder(methodName)
-          .addModifiers(Modifier.PUBLIC)
-          .returns(returnType);
+            final List<String> paramNames = me.getParameters().stream().map(ve -> {
+                methodBuilder.addParameter(TypeName.get(ve.asType()), ve.getSimpleName().toString());
+                return ve.getSimpleName().toString();
+            }).collect(Collectors.toList());
 
-      List<String> paramNames = me.getParameters().stream().map(ve -> {
-        methodBuilder.addParameter(TypeName.get(ve.asType()), ve.getSimpleName().toString());
-        return ve.getSimpleName().toString();
-      }).collect(Collectors.toList());
+            final InvocationGenerator invocation = new InvocationGenerator(packageName, className(), me);
+            methodBuilder.addParameter(ClassName.get(packageName, className(), invocation.className()),
+                    "ignored");
+            me.getThrownTypes().forEach(et -> methodBuilder.addException(TypeName.get(et)));
 
-      InvocationGenerator invocation = new InvocationGenerator(packageName, className(), me);
-      methodBuilder.addParameter(ClassName.get(packageName, className(), invocation.className()), "ignored");
-      me.getThrownTypes().forEach(et -> methodBuilder.addException(TypeName.get(et)));
+            wrapClass.addMethod(methodBuilder.addStatement((isVoid ? "" : "return ") + "service.$L($L)",
+                    methodName, String.join(", ", paramNames)).build());
+        });
+        return wrapClass.build();
+    }
 
-      wrapClass.addMethod(
-          methodBuilder
-              .addStatement((isVoid ? "" : "return ") + "service.$L($L)", methodName, String.join(", ", paramNames))
-              .build());
-    });
-    return wrapClass.build();
-  }
+    private ClassName classType() {
+        return ClassName.get(packageName, className());
+    }
 
-  private ClassName classType() {
-    return ClassName.get(packageName, className());
-  }
+    String className() {
+        final Middleware anno = element.getAnnotation(Middleware.class);
+        return anno.name().length() > 0 ? anno.name() : originClassName + "Middleware";
+    }
 
-  String className() {
-    Middleware anno = element.getAnnotation(Middleware.class);
-    return anno.name().length() > 0 ? anno.name() : originClassName + "Middleware";
-  }
-
-  String getFullName() {
-    return packageName + "." + className();
-  }
+    String getFullName() {
+        return packageName + "." + className();
+    }
 }
